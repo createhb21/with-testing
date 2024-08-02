66602971ffc446d6fb9ff8d9bfce901eb2386c6f
hyobumcodi <hb.lee@bodycodi.com>
2024-08-02 13:11:18 +0900

    change ci 8

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,64 +3,73 @@ module.exports = {
   env: {
     es6: true,
     browser: true,
-    jest: true
+    jest: true,
   },
-  ignorePatterns: ["node_modules/", ".pnp.cjs", ".pnp.loader.cjs", "public/"],
+  ignorePatterns: ['node_modules/', 'public/'],
   extends: [
-    "airbnb",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:@next/next/recommended"
+    'airbnb',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:@next/next/recommended',
   ],
-  plugins: [],
+  plugins: ['unused-imports'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module"
-  },
-  settings: {
-    "import/resolver": {
-      alias: {
-        map: [["@", "./src"]],
-        extensions: [".ts", ".js", ".tsx", ".json"]
-      }
-    }
+    sourceType: 'module',
   },
   overrides: [
     {
-      files: ["*.ts", "*.tsx"],
-      extends: ["airbnb-typescript", "plugin:@typescript-eslint/recommended"],
-      plugins: ["@typescript-eslint"],
+      files: ['*.ts', '*.tsx'],
+      extends: ['airbnb-typescript', 'plugin:@typescript-eslint/recommended'],
+      plugins: ['@typescript-eslint'],
       rules: {},
-      parser: "@typescript-eslint/parser",
+      parser: '@typescript-eslint/parser',
       parserOptions: {
-        project: ["./tsconfig.json"]
-      }
+        project: ['./tsconfig.json'],
+      },
     },
     {
-      files: ["src/hooks/**/**/*.test.ts?(x)"],
+      files: ['src/hooks/**/**/*.test.ts?(x)'],
       rules: {
-        "react-hooks/rules-of-hooks": "off"
-      }
+        'react-hooks/rules-of-hooks': 'off',
+      },
     },
     {
-      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-      extends: ["plugin:testing-library/react", "plugin:jest/recommended"],
-      rules: {}
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react', 'plugin:jest/recommended'],
     },
     {
-      extends: ["plugin:cypress/recommended"],
-      files: ["cypress/**/*.ts"],
-      parser: "@typescript-eslint/parser",
+      extends: ['plugin:cypress/recommended'],
+      files: ['cypress/**/*.ts'],
+      parser: '@typescript-eslint/parser',
       parserOptions: {
-        project: ["./cypress/tsconfig.json"]
-      }
-    }
+        project: ['./cypress/tsconfig.json'],
+      },
+    },
   ],
-  rules: {}
+  rules: {
+    'no-unused-vars': 'off',
+    'react/no-danger': 'off',
+    'object-curly-newline': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-uses-vars': 'error',
+    'react/jsx-uses-react': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
 };
